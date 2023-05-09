2cbb7024a3c8633f6d9a5751e2606e5a4be3cbf8
Niallybrown <niallfaucher@gmail.com>
2023-05-09 16:39:33 +0100

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,42 @@ const contactSection = document.getElementById("contactSection");
 
 const sections = [logoSection, aboutSection, servicesSection1, servicesSection2, contactSection];
 
-
+let sectionObjects = sections.map(section => ({
+	offsetTop: section.offsetTop,
+	clientHeight: section.clientHeight
+}))
 
 document.getElementById("scroll-button").addEventListener("click", () => {
-    sections[sectionIndex + 1].scrollIntoView({behavior: "smooth"});
-    sectionIndex += 1;
-})
+	sections[sectionIndex + 1].scrollIntoView({behavior: "smooth"});
+	sectionIndex += 1;
+});
+
+function logScroll() {
+	const scrollY = window.scrollY;
+	const scrollButton = document.getElementById("scroll-button");
+
+	sectionObjects.forEach((section, index) => {
+		if(scrollY >= section.offsetTop && scrollY < (section.offsetTop + (section.clientHeight / 2))) {
+			sectionIndex = index;
+		}
+		if(sectionIndex === sectionObjects.length - 1) {
+				scrollButton.style.display = "none";
+		} else if(scrollButton.style.display === "none") {
+			scrollButton.style.display = "block";
+		}
+	});
+}
+
+const debounce = (callback, wait) => {
+  let timeoutId = null;
+  return (...args) => {
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      callback.apply(null, args);
+    }, wait);
+  };
+}
+
+
+
+document.addEventListener('scroll', debounce(logScroll, 500));
